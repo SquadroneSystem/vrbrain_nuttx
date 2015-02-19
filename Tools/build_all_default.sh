e22ee93cc56b2acb25a83410e12051776afb71e4
LukeMike <luca.micheletti.laser@gmail.com>
2015-02-19 16:07:05 +0100

    TOOLS: new scripts to build all archives and all default NuttX firmware

---
+++ Tools/build_all_default.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+make clean
+
+make hbrain-v10_default
+make vrbrain-v45_default
+
+make vrbrain-v51_default
+
+make vrbrain-v51Pro_default
+
+make vrbrain-v52_default
+
+make vrbrain-v52Pro_default
+
+make vrubrain-v51_default
+
+make vrubrain-v52_default
